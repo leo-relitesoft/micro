01ada9c76479df8e20e67c2373c166d7caa96a9f
leo <leo.relitesoft@mail.ru>
2026-06-10 20:34:44 +0300

    composer.json cleanup

--- public/index.php
+++ public/index.php
@@ -19,7 +19,6 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(false, true, true);
 
 $app->get('/{id}', function (Request $request, Response $response, $args){
-    throw new HttpEncodingException('error');
     $res = RequirementsCheck::getMissingModules();
     $response->getBody()->write(json_encode($res, JSON_PRETTY_PRINT));
     return $response->withHeader('Content-type', 'application/json');
