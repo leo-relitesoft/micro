de9fede3f54c7c99f04a83d0fa26ecc8f2fe09f1
leo <leo.relitesoft@mail.ru>
2026-06-11 13:16:09 +0300

    JsonResponse.php

--- src/Http/HomeAction.php
+++ src/Http/HomeAction.php
@@ -10,23 +10,13 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class HomeAction implements RequestHandlerInterface
 {
-    private ResponseFactoryInterface $factory;
-    /**
-     * HomeAction constructor.
-     * @param ResponseFactoryInterface $factory
-     */
-    public function __construct(ResponseFactoryInterface $factory)
-    {
-        $this->factory = $factory;
-    }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $response = $this->factory->createResponse();
         $res = RequirementsCheck::getMissingModules();
         if (getenv('APP_DEBUG')) {
             $res[] = "DEBUG MODE";
         }
-        return Http::json($response, $res);
+        return new JsonResponse($res);
     }
 }
---
+++ src/Http/JsonResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http;
+
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\StreamInterface;
+use Slim\Psr7\Interfaces\HeadersInterface;
+use Slim\Psr7\Response;
+
+class JsonResponse extends Response
+{
+    public function __construct($data, int $status = StatusCodeInterface::STATUS_OK, ?HeadersInterface $headers = null, ?StreamInterface $body = null)
+    {
+        parent::__construct($status, $headers, $body);
+        $this->headers->addHeader('Content-type', 'application/json');
+        $this->body->write(json_encode($data, JSON_THROW_ON_ERROR));
+    }
+}
